21b6fc46a1569589f680698a2a9e67a93b11cbe8
gyantik <kgyanti54@gmail.com>
2026-03-11 22:52:14 +0530

    solved: sumOfSequence - Recursion

--- RECURSION/sumOfSequence.java
+++ RECURSION/sumOfSequence.java
@@ -1,6 +1,19 @@
 // LeetCode: sumOfSequence 
 // Date: 11-03-2026 
  
-class Solution { 
-    // paste your solution here 
-} 
+class Solution {
+    public int countSubsequenceWithTargetSum(int[] nums, int k) {
+        return  solve(nums,0,0,k);
+    }
+   public int solve(int[] nums,int idx,int sum,int k){
+        if(idx==nums.length){
+           if(sum==k){
+            return 1;
+           }
+           return 0;
+        }
+        int take = solve(nums,idx+1,sum+nums[idx],k);
+        int notTake=solve(nums,idx+1,sum,k);
+        return take + notTake;
+    }
+}
